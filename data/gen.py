878abe399c184a795483b09c6371c8bb99c465e8
JunhaoQiu <56094690+qchiujunhao@users.noreply.github.com>
2022-10-24 10:29:40 -0400

    seems finished the task

--- data/gen.py
+++ data/gen.py
@@ -2,8 +2,9 @@ import csv
 def auto_gen():
     header = ['name', 'area', 'country_code2', 'country_code3']
     data = ['Afghanistan', 652090, 'AF', 'AFG']
+    data2 = ['Afghanistansdss', 652090, 'AF', 'AFG']
 
-    with open('countries.csv', 'w', encoding='UTF8') as f:
+    with open('hello_publish/countries.csv', 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
 
     # write the header
@@ -11,6 +12,8 @@ def auto_gen():
 
     # write the data
         writer.writerow(data)
+        writer.writerow(data2)
+
 
 if __name__ == "__main__":
     print("good here")
--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import os
+path = os.getcwd()
+os.system("sh "+path+"/data/gen.sh")
 
 class Install_with_sym(install):
     def run(self):
         install.run(self)
-        path = os.getcwd()
-
 setup(
     name='hello_publish',
-    version='0.1.0',
-    packages=find_packages(include=['hello_publish', 'hello_publish.*'])
+    version='0.1.2',
+    cmdclass={'install': Install_with_sym},
+    packages=find_packages(include=['hello_publish', 'hello_publish.*']),
+    scripts=['data/gen.sh'],
+    include_package_data=True,
+    package_data={'hello_publish': ['countries.csv']}
 )
+
+# setup(name='Tzara---A-Personal-Assistant',
+#       version='1.0.6',
+#       description='A Virtual Personal Assistant',
+#       url='https://github.com/Suman7495/Tzara---A-Personal-Assistant',
+#       author='MyName',
+#       author_email='myemail@gmail.com',
+#       license='MIT',
+#       packages=['tzara'],
+#       classifiers=['Development Status :: 4 - Beta',
+#       'Programming Language :: Python :: 2.7',            
+#       ],
+#       scripts=['bin/desktopsetup.sh', 'bin/startup.sh'],
+#       cmdclass={'install': MyInstall},
+# )
